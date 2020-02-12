d1f7befa950edd6fae7ddadecf3fca9fd481a440
Manu Kanwarpal <makanw@microsoft.com>
2020-02-12 22:56:25 +0000

    formatting

--- Custom.js
+++ Custom.js
@@ -5,7 +5,7 @@
 function init() {
     can = document.getElementById("MyCanvas1");
     ctx = can.getContext("2d");
-    ctx.fillStyle = "Brown";
+    ctx.fillStyle = "White";
     ctx.font = "30pt Comic";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
@@ -20,7 +20,7 @@ function RunTextLeftToRight() {
     ctx.clearRect(0, 0, can.width, can.height);
     ctx.save();
     ctx.translate(step, can.height / 2);
-    ctx.fillText("Parth is turning One!!", 0, 0);
+    ctx.fillText("Parth is turning five :)", 0, 0);
     ctx.restore();
     if (step == steps)
         step = 320;
